07e6d6ea974c8ed79523081adc5407f6ea70a345
Lycanthrope <Taszidizaz@iut-dhaka.edu>
2023-10-01 01:33:40 +0600

    made minor changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,34 +3,34 @@ import SiteNavBar from "@/components/SiteNavBar";
 import SiteFooter from "@/components/SiteFooter";
 import NextTopLoader from 'nextjs-toploader';
 
-async function fetchMetaData() {
-  const res = await fetch(process.env.BASE_URL + "api/MetaData");
-  if (!res.ok) {
-    throw new Error("MetaData Fetching Failed");
-  }
-  return res.json();
-}
+// async function fetchMetaData() {
+//   const res = await fetch(process.env.BASE_URL + "api/MetaData");
+//   if (!res.ok) {
+//     throw new Error("MetaData Fetching Failed");
+//   }
+//   return res.json();
+// }
 
 export default async function RootLayout({ children }) {
-  let metaData;
+  // let metaData;
 
-  try {
-    metaData = await fetchMetaData();
-  } catch (error) {
-    console.error("Error fetching meta data:", error);
+  // try {
+  //   metaData = await fetchMetaData();
+  // } catch (error) {
+  //   console.error("Error fetching meta data:", error);
 
-    metaData = {
-      title: "Home",
-      description: "This is the home page."
-    };
-  }
+  //   metaData = {
+  //     title: "Home",
+  //     description: "This is the home page."
+  //   };
+  // }
 
   return (
     <html lang="en">
       <head>
-        <title>{metaData.title}</title>
+        {/* <title>{metaData.title}</title>
         <meta name="description" content={metaData.description} />
-        <meta name="keywords" content={metaData.keywords} />
+        <meta name="keywords" content={metaData.keywords} /> */}
         
       </head>
       <body>
